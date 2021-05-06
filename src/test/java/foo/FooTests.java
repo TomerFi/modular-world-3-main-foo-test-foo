932e5ee2626e33e3c274af9eb11f030ff6f5ee04
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-05-06 13:42:43 +0300

    build: initial build, cannot access encapsulated resource files

---
+++ src/test/java/foo/FooTests.java
@@ -0,0 +1,28 @@
+package foo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class FooTests {
+
+  @Test
+  void accessClassFoo() {
+    assertEquals("Foo", new Foo().getClass().getSimpleName(), "Simple name should be 'Foo'!");
+  }
+
+  @Test
+  void accessModuleFoo() {
+    assertEquals("foo", getClass().getModule().getName(), "Class doesn't reside in module 'foo'!");
+  }
+
+  @Test
+  void getMainResourceViaClassLoader() {
+    assertNotNull(Foo.class.getClassLoader().getResource("foo/sub/main.txt"));
+  }
+
+  @Test
+  void getTestResourceViaClassLoader() {
+    assertNotNull(Foo.class.getClassLoader().getResource("foo/sub/test.txt"));
+  }
+}
